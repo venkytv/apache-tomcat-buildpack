ef1c752471a2e9e5a30c8ec8ffae09445af96e88
happiestminds-covisint <smlingesh@rediffmail.com>
2015-01-27 16:40:02 +0530

    Update startup.sh

--- bin/startup.sh
+++ bin/startup.sh
@@ -1,4 +1,4 @@
 
 echo "STARTING TOMCAT ---------> $(netstat -a)"
-JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=8080" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run #& >> /dev/null 2>&1
+JAVA_HOME=$HOME/jdk1.8.0_25 JAVA_OPTS="-Djava.io.tmpdir=$TMPDIR -Dhttp.port=8080" $HOME/apache-tomcat-7.0.57/bin/catalina.sh run & >> /dev/null 2>&1
 echo "TOMCAT STARTED -------> $(netstat -a)"
